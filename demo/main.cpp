0bcc6aa3013b95be092a8bc707f415956de24d35
Brett Weir <brett@lamestation.com>
2015-03-25 06:49:11 -0700

    Remove unneeded code

--- demo/main.cpp
+++ demo/main.cpp
@@ -12,11 +12,6 @@ int main(int argc, char *argv[])
     MemoryMap * map = new MemoryMap;
     map->loadFile("LameSerial.binary");
 
-    QFont f = QFont("Courier",14);
-    f.setFixedPitch(true);
-    f.setKerning(false);
-    map->setFont(f);
-
     map->show();
 
     return app.exec();
