52155a09660ba391f6319285e2e9068325877d8e
Brett Weir <brett@lamestation.com>
2016-04-08 04:13:19 -0700

    Pull applet colors from main IDE colors then use Ice defaults

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,7 @@
 #include <QApplication>
 #include <QDebug>
 #include <QWidget>
+#include <QSettings>
 
 #include <MemoryMap>
 #include <PropellerManager>
@@ -10,11 +11,27 @@ int main(int argc, char *argv[])
 
     QApplication app(argc, argv);
 
+    QCoreApplication::setOrganizationName("Parallax");
+    QCoreApplication::setApplicationName("PropellerIDE");
+
     PropellerManager manager;
     MemoryMap * map = new MemoryMap(&manager);
 
     if (argc > 1)
         map->openFile(argv[1]);
+
+    QSettings settings;
+    settings.beginGroup("Colors");
+
+    map->recolor(
+            settings.value("Block_PUB",         "#dde3f3").toString(),
+            settings.value("Block_DAT",         "#e6f5e9").toString(),
+            settings.value("Syntax_Text",       "#404040").toString(),
+            settings.value("Syntax_Keywords",   "#003c79").toString(),
+            settings.value("Block_CON",         "#f8f8f8").toString(),
+            settings.value("Syntax_Comments",   "#999999").toString(),
+            settings.value("Syntax_Text",       "#404040").toString()
+           );
 
     map->show();
 
